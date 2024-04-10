27056da6f4d39ac747beceb2d4dbd759d32296c9
Yorth Ortegon <yorth21@gmail.com>
2024-04-09 20:39:33 -0500

    Refactor: modificar import Main

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Constants from 'expo-constants'
 import { View } from 'react-native';
-import Main from './src/components/Main.jsx'
+import Main from './src/Main.jsx'
 
 export default function App() {
   return (
